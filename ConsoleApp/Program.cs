f1e1d47900e6285943787384e20e873f7dea4e83
IDT-CORALVILLE\jvolker <jvolker@idtdna.com>
2019-01-11 13:40:12 -0600

    removing boilerplate code

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,21 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-	class Program
+	internal static class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
-			// The code provided will print ‘Hello World’ to the console.
-			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 			Console.WriteLine("Hello World!");
 			Console.ReadKey();
-
-			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
 		}
 	}
 }
